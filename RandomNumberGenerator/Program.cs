571c1251262d93ae286b0d739f13d35ea3f557e5
Rida Serroukh <rida.serroukh@gmail.com>
2022-03-06 23:23:04 +0000

    Add project files.

---
+++ RandomNumberGenerator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RandomNumberGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random num = new Random();
+            int guess;
+            int actualNumber = num.Next(1, 100);
+      
+            Console.WriteLine("Guess a number from 1 to 100: ");
+            guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess == actualNumber)
+            {
+                Console.WriteLine("YOU WIN!");
+            }
+            if (guess > actualNumber)
+            {
+                Console.WriteLine("Your guess is too high, the actual number is: "+actualNumber);
+            }
+            if (guess < actualNumber)
+            {
+                Console.WriteLine("Your guess is too low, the actual number is: "+actualNumber);
+            }
+        }
+    }
+}
